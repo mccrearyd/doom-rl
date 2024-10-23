9e2a0c559e6bd5afc24f8f3852cd6ecc0ba9528e
verbose-void <dyllanmccreary@gmail.com>
2024-10-22 21:08:22 -0700

    setting up interactor

--- interactor.py
+++ interactor.py
@@ -1,4 +1,26 @@
+import torch
+import numpy as np
 
+import gymnasium
+from gymnasium.vector.utils import batch_space
+
+
+class VizDoomVectorized:
+    def __init__(self, num_envs: int):
+        self.num_envs = num_envs
+
+    def reset(self):
+        # TODO: reset all envs and return the observations in one tensor
+        pass
+
+    def step(self, actions):
+        # TODO: this method should accept the actions and return the observations and rewards in two tensors.
+        # it should step the environments in parallel. we will be calling step() as many times as we can in a
+        # vectorized episode.
+
+        # could you also make it such that if the environment is done, it resets itself?
+        pass
+    
 
 
 class Interactor:
@@ -8,5 +30,18 @@ class Interactor:
     internal vectorization, making gradients easier to accumulate.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, num_envs: int):
+        self.num_envs = num_envs
+
+        self.env = None  # TODO
+        self.action_space = batch_space(self.env.action_space, self.num_envs)
+
+    def step(self):
+        # for now, let's just use action_space.sample()
+
+        observations = torch.zeros(size=(self.num_envs, 240, 320, 3), dtype=torch.uint8)
+
+        # HACK: pretend like we do an agent.forward(observations)
+        actions = self.env.action_space.sample()
+
+        
