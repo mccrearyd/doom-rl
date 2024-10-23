f02bfa48f7245314b5164c29cb48d4b9cb46316f
verbose-void <dyllanmccreary@gmail.com>
2024-10-22 21:00:00 -0700

    base interactor

---
+++ interactor.py
@@ -0,0 +1,12 @@
+
+
+
+class Interactor:
+    """This thing manages the state of the environment and uses the agent
+    to infer and step on the environment. This way is a bit easier
+    because we can have environment mp while relying on the agent's
+    internal vectorization, making gradients easier to accumulate.
+    """
+
+    def __init__(self):
+        pass
