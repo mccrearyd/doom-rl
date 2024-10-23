06e52d2235d344bf091f49271b15305b07be69fc
verbose-void <dyllanmccreary@gmail.com>
2024-10-22 20:37:54 -0700

    display to screen

--- doom.py
+++ doom.py
@@ -1,12 +1,23 @@
 import gymnasium
 from vizdoom import gymnasium_wrapper
 env = gymnasium.make("VizdoomCorridor-v0")
+
 observation, info = env.reset()
+
+# let's do a cv2 window to show the screen
+import cv2
+cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("screen", 640, 480)
+
 for _ in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     print(observation["screen"].shape, reward)
     print(observation["gamevariables"].shape, reward)
+
+    # show the screen
+    cv2.imshow("screen", observation["screen"])
+    cv2.waitKey(1)
 
     if terminated or truncated:
         observation, info = env.reset()
