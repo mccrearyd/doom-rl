c4f480b7a03129d332d605d113d3666f60d3d1da
verbose-void <dyllanmccreary@gmail.com>
2024-10-22 21:17:36 -0700

    add train_doom.py

--- train_doom.py
+++ train_doom.py
@@ -1,13 +1,24 @@
-from doom_vec import play_doom
-
-
+from interactor import DoomInteractor
 
 
 
 if __name__ == "__main__":
-    NUM_ENVS = 8
-    MAX_FRAMES = 100
+    MAX_STEPS = 100
+    NUM_ENVS = 16
+    
+    # if true one of the environments will be displayed in a cv2 window
+    WATCH = False
+    
+    interactor = DoomInteractor(NUM_ENVS, watch=WATCH)
 
-    # obs shape (240, 320, 3)
-    # uint8
-    play_doom(num_envs=NUM_ENVS, max_frames=MAX_FRAMES)
+    # Reset all environments
+    observations = interactor.env.reset()
+    # print("Initial Observations:", observations.shape)
+
+    # Example of stepping through the environments
+    for _ in range(100):  # Step for 100 frames or episodes
+        observations, rewards, dones = interactor.step()
+        print(observations.shape, rewards.shape)
+
+    # Close all environments
+    interactor.env.close()
