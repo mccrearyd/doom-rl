fb0ed9d7c4cfb866a255b287b99d058fb47dfea1
verbose-void <dyllanmccreary@gmail.com>
2024-10-25 17:22:29 -0700

    better reward function code (VizDoomRewardFeatures)

--- custom_doom.py
+++ custom_doom.py
@@ -14,11 +14,62 @@ register(
     kwargs={"scenario_file": scenario_file},
 )
 
+from dataclasses import dataclass
+
+@dataclass
+class VizDoomRewardFeatures:
+    KILLCOUNT: int
+    ITEMCOUNT: int
+    SECRETCOUNT: int
+    FRAGCOUNT: int
+    DEATHCOUNT: int
+    HITCOUNT: int
+    HITS_TAKEN: int
+    DAMAGECOUNT: int
+    DAMAGE_TAKEN: int
+    HEALTH: int
+    ARMOR: int
+    DEAD: int
+    SELECTED_WEAPON_AMMO: int
+    POSITION_X: int
+    POSITION_Y: int
+    POSITION_Z: int
+
+    @classmethod
+    def make_from_game(cls, game):
+        return cls(
+            KILLCOUNT=game.get_game_variable(vzd.GameVariable.KILLCOUNT),
+            ITEMCOUNT=game.get_game_variable(vzd.GameVariable.ITEMCOUNT),
+            SECRETCOUNT=game.get_game_variable(vzd.GameVariable.SECRETCOUNT),
+            FRAGCOUNT=game.get_game_variable(vzd.GameVariable.FRAGCOUNT),
+            DEATHCOUNT=game.get_game_variable(vzd.GameVariable.DEATHCOUNT),
+            HITCOUNT=game.get_game_variable(vzd.GameVariable.HITCOUNT),
+            HITS_TAKEN=game.get_game_variable(vzd.GameVariable.HITS_TAKEN),
+            DAMAGECOUNT=game.get_game_variable(vzd.GameVariable.DAMAGECOUNT),
+            DAMAGE_TAKEN=game.get_game_variable(vzd.GameVariable.DAMAGE_TAKEN),
+            HEALTH=game.get_game_variable(vzd.GameVariable.HEALTH),
+            ARMOR=game.get_game_variable(vzd.GameVariable.ARMOR),
+            DEAD=game.get_game_variable(vzd.GameVariable.DEAD),
+            SELECTED_WEAPON_AMMO=game.get_game_variable(vzd.GameVariable.SELECTED_WEAPON_AMMO),
+            POSITION_X=game.get_game_variable(vzd.GameVariable.POSITION_X),
+            POSITION_Y=game.get_game_variable(vzd.GameVariable.POSITION_Y),
+            POSITION_Z=game.get_game_variable(vzd.GameVariable.POSITION_Z),
+        )
+
+    def get_deltas(self, other: "VizDoomRewardFeatures") -> "VizDoomRewardFeatures":
+        # loop through all our fields and subtract the other fields from them
+        field_names = self.__annotations__.keys()
+        return VizDoomRewardFeatures(
+            **{field: getattr(self, field) - getattr(other, field) for field in field_names}
+        )
+
+
 class VizDoomCustom:
     def __init__(self):
         self.env = gymnasium.make("VizdoomCustom-v0")
         self.game = self.env.env.env.game
-        self.prev_state = None
+        self._prev_reward_features = None
+        self._current_reward_features = None
 
     @property
     def action_space(self):
@@ -30,53 +81,39 @@ class VizDoomCustom:
 
     def reset(self):
         observation, info = self.env.reset()
-        self.prev_state = self._get_state()
+        self._prev_reward_features = self._get_reward_features()
         return observation, info
 
     def step(self, action):
         # Execute the action and observe the next state
         observation, _, terminated, truncated, info = self.env.step(action)
-        next_state = self._get_state()
+        self._current_reward_features = self._get_reward_features()
 
         # Calculate custom reward
-        reward = self._get_reward(self.prev_state, next_state)
-        self.prev_state = next_state  # Update previous state
+        reward = self._get_reward()
+        self._prev_reward_features = self._current_reward_features  # Update previous state
 
         return observation, reward, terminated, truncated, info
 
-    def _get_state(self):
-        return (
-            self.game.get_game_variable(vzd.GameVariable.KILLCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.ITEMCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.SECRETCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.FRAGCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.DEATHCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.HITCOUNT),
-            self.game.get_game_variable(vzd.GameVariable.HITS_TAKEN),
-            self.game.get_game_variable(vzd.GameVariable.DAMAGECOUNT),
-            self.game.get_game_variable(vzd.GameVariable.DAMAGE_TAKEN),
-            self.game.get_game_variable(vzd.GameVariable.HEALTH),
-            self.game.get_game_variable(vzd.GameVariable.ARMOR),
-            self.game.get_game_variable(vzd.GameVariable.DEAD),
-            self.game.get_game_variable(vzd.GameVariable.SELECTED_WEAPON_AMMO),
-            self.game.get_game_variable(vzd.GameVariable.POSITION_X),
-            self.game.get_game_variable(vzd.GameVariable.POSITION_Y),
-            self.game.get_game_variable(vzd.GameVariable.POSITION_Z),
-        )
-    
-        # TODO: for some reason, picking up guns results in a large negative reward.
+    def _get_reward_features(self) -> VizDoomRewardFeatures:
+        return VizDoomRewardFeatures.make_from_game(self.game)
 
-    def _get_reward(self, prev_state, next_state):
+    def _get_reward(self):
         reward = 0
-        
-        # Reward calculations based on state changes
-        reward += (next_state[5] - prev_state[5]) * 300  # Enemy hit
-        reward += (next_state[0] - prev_state[0]) * 1000  # Enemy kill
-        reward += (next_state[1] - prev_state[1]) * 100  # Item pick up
-        reward += (next_state[2] - prev_state[2]) * 500  # Secret found
-        reward += (next_state[9] - prev_state[9]) * 10  # Health delta
-        reward += (next_state[10] - prev_state[10]) * 10  # Armor delta
-        reward += (next_state[12] - prev_state[12]) * 10  # Ammo delta
+
+        if self._prev_reward_features is None:
+            return reward
+
+        # get deltas
+        deltas = self._current_reward_features.get_deltas(self._prev_reward_features)
+
+        reward += deltas.KILLCOUNT * 1000
+        reward += deltas.ITEMCOUNT * 100
+        reward += deltas.SECRETCOUNT * 500
+        reward += deltas.HITCOUNT * 300
+        reward += deltas.HEALTH * 10
+        reward += deltas.ARMOR * 10
+        reward += deltas.SELECTED_WEAPON_AMMO * 10
 
         return reward
 
